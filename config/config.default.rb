a0b01ecb158bafc3e442b6cce0212458b6da9610
YorickPeterse <info@yorickpeterse.com>
2011-06-01 12:19:19 +0200

    Prepared the website for Zen 0.2.6.1.

--- config/config.default.rb
+++ config/config.default.rb
@@ -1,9 +1,12 @@
+require __DIR__('../vendor/ramaze/cache/memcache')
+
 # Specify the root directory. This is required since there are multiple directories
 # to load resources from. This directory will be used for the database logger, modes, etc.
 Zen.root = __DIR__('../')
 
 # Set the application's mode. Available modes are "dev" and "live"
-Ramaze.options.mode  = :dev
+Ramaze.options.mode     = :dev
+Ramaze.options.app.name = :'yorickpeterse.com'
 
 # The session identifier to use for cookies.
 Ramaze.options.session.key = 'yorickpeterse.com.sid'
@@ -14,4 +17,5 @@ Ramaze::View.options.cache      = false
 Ramaze::View.options.read_cache = false
 
 # Use LRU instead of the memory cache as the latter leaks memory over time
-Ramaze::Cache.options.session = Ramaze::Cache::LRU
+Ramaze::Cache.options.session  = Ramaze::Cache::MemCacheBackport
+Ramaze::Cache.options.settings = Ramaze::Cache::MemCacheBackport
