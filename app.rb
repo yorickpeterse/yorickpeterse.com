34eb70263d653522d28b8846d7a6fda444b67a18
Yorick Peterse <yorickpeterse@gmail.com>
2011-11-26 18:16:04 +0100

    Small code cleanups
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- app.rb
+++ app.rb
@@ -1,21 +1,10 @@
-# Load the Zen gem
 require 'zen'
 require 'rdiscount'
 
-# Load the configuration files
 require __DIR__('config/config')
-
-# Load all our custom Rack middlewares
 require __DIR__('config/middlewares')
-
-# Load the database configuration file
 require __DIR__('config/database')
-
-# Load all routes
 require __DIR__('config/routes')
-
-# Make sure that Ramaze knows where you are
-Ramaze.options.roots.push(Zen.root)
 
 # Require all the custom gems/modules we need
 require __DIR__('theme/yorickpeterse')
