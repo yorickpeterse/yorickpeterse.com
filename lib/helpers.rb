c42f13771b92ee4d3267e6e9c2f2a4bb35e9d64c
Yorick Peterse <yorickpeterse@gmail.com>
2014-02-13 20:42:08 +0100

    Simplified timestamp generation even more.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,24 +2,16 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Rendering
 
 ##
-# Returns a String containing the date in a fixed format.
-#
-# @param [String] date
-# @return [String]
-#
-def format_date(date)
-  return attribute_to_time(date).dup.utc.iso8601
-end
-
-##
 # Returns a `<time>` tag for the given date string.
 #
 # @param [String] date
 # @return [String]
 #
 def date_tag(date)
-  human_format   = attribute_to_time(date).to_s
-  machine_format = format_date(date)
+  date = attribute_to_time(date)
+
+  machine_format = date.iso8601
+  human_format   = date.dup.utc.strftime('%Y-%m-%d %R %Z')
 
   return '<time datetime="%s">%s</time>' % [machine_format, human_format]
 end
